9e2f5c6b96c5fc7fd08e2378001e1637a9123306
Admin-Host <admin@admin-host.com>
2024-12-23 13:03:18 -0500

    Send data to backend server via POST request

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+const express = require('express'); // Import the express library
+const app = express(); // Create an express app
+const port = 8080; // Set the app port
 
-app.set('view engine', 'ejs');
+app.use(express.json()); // Allow for JSON POST bodies
 
-app.get('/', (req, res) => {
-    res.render('index', { message: 'Hello World!' });
+app.set('view engine', 'ejs'); // Set the view engine to EJS
+
+app.get('/', (req, res) => { // Handle GET requests to the root URL
+    res.render('index', { message: 'Hello World!' }); // Render the index.ejs file
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res) => { // Handle POST requests to the root URL
     console.log('Got a POST request');
+    if (req.body && Object.keys(req.body).length > 0) { // Check if the POST request has data
+        console.log('Received data:', req.body); // Log the data to the console
+        res.json({ message: 'POST request received' });
+    } else {
+        res.json({ message: 'POST request received, but with no data. Check that your domain is valid and registered with LENS' });
+    };
 });
 
 app.listen(port, () => {
-    console.log(`LENS App listening at http://localhost:${port}`);
+    console.log(`LENS App listening at http://localhost:${port}`); // Log the app status to the console
 });
